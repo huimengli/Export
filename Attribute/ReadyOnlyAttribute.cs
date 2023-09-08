ae8a170b6dd437aace8f96e172ef8b722917bdde
zimingdehua <zimingdehua@163.com>
2023-09-08 11:51:58 +0800

    添加了ReadyOnly的属性,添加了dll属性

---
+++ Attribute/ReadyOnlyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Export.Attribute
+{
+    /// <summary>
+    /// 编辑器只读(内容无法在在编辑器中修改)
+    /// </summary>
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    } 
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("Unity常用模块")]
+[assembly: AssemblyDescription("Unity个人使用时常用的模块")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("绘绘的小家")]
+[assembly: AssemblyProduct("UnityModel")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//将 ComVisible 设置为 false 将使此程序集中的类型
+//对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("36eca5a5-7dab-4541-a46d-4d10365bb0f8")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+[assembly: AssemblyVersion("0.1.0.2")]
+[assembly: AssemblyFileVersion("0.1.0.2")]
+[assembly: NeutralResourcesLanguage("zh")]
+
