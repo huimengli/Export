65da3d5e4c985b462995ce976d9a65fa14f5aed6
绘梦璃 <2913394404@qq.com>
2023-10-04 16:07:44 +0800

    添加了一个Message模块 没什么用,就是给unity对象添加注释用的

---
+++ Tools/Message.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Export.Tools
+{
+    /// <summary>
+    /// 消息框
+    /// </summary>
+    public class Message : MonoBehaviour
+    {
+        /// <summary>
+        /// 这里不写内容
+        /// 是丢给编译器给别人看消息用的
+        /// </summary>
+        public string value;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    } 
+}
