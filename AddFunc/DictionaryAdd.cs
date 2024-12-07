6626af38e29ab684b869b527a5da382d0a69eb40
绘梦璃 <2913394404@qq.com>
2024-12-07 23:50:45 +0800

    修复错误

--- AddFunc/DictionaryAdd.cs
+++ AddFunc/DictionaryAdd.cs
@@ -17,10 +17,22 @@ namespace Export.AddFunc
         /// <typeparam name="TValue"></typeparam>
         /// <param name="dict"></param>
         /// <returns></returns>
-        public static List<TKey> KeysToArray<TKey,TValue>(this Dictionary<TKey,TValue> dict)
+        public static List<TKey> KeysToList<TKey,TValue>(this Dictionary<TKey,TValue> dict)
         {
             var keys = dict.Keys;
             return new List<TKey>(keys);
+        }
+
+        /// <summary>
+        /// 键组
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dict"></param>
+        /// <returns></returns>
+        public static TKey[] KeysToArray<TKey,TValue>(this Dictionary<TKey,TValue> dict)
+        {
+            return dict.KeysToList().ToArray();
         }
 
         /// <summary>
@@ -30,10 +42,22 @@ namespace Export.AddFunc
         /// <typeparam name="TValue"></typeparam>
         /// <param name="dict"></param>
         /// <returns></returns>
-        public static List<TValue> ValuesToArray<TKey,TValue>(this Dictionary<TKey,TValue> dict)
+        public static List<TValue> ValuesToList<TKey,TValue>(this Dictionary<TKey,TValue> dict)
         {
             var values = dict.Values;
             return new List<TValue>(values);
+        }
+
+        /// <summary>
+        /// 值组
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dict"></param>
+        /// <returns></returns>
+        public static TValue[] ValuesToArray<TKey,TValue>(this Dictionary<TKey,TValue> dict)
+        {
+            return dict.ValuesToList().ToArray();
         }
     }
 }
